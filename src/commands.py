34b19a3bc5032cef4eb150ef899b0802a20740cd
Andrew <andrew.g.93@hotmail.com>
2020-11-15 17:33:58 +1100

    Added seed command using faker

--- src/commands.py
+++ src/commands.py
@@ -1,6 +1,8 @@
+from faker import Faker
 from flask import Blueprint
 
 from src.main import db
+from src.models.Item import Item
 
 
 db_commands = Blueprint("db", __name__)
@@ -14,3 +16,15 @@ def create_db():
 def drop_db():
     db.drop_all()
     print("TABLES DROPPED")
+
+@db_commands.cli.command("seed")
+def seed_db():
+    faker = Faker()
+
+    for i in range(10):
+        item = Item()
+        item.name = faker.catch_phrase()
+        db.session.add(item)
+    
+    db.session.commit()
+    print("TABLES SEEDED")

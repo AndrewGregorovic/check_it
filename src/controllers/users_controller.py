ad9a0c360c816e34d5c176726b1fd41aba0846b2
Andrew <andrew.g.93@hotmail.com>
2020-11-22 18:18:07 +1100

    Added routes for uploading user profile images

--- src/controllers/users_controller.py
+++ src/controllers/users_controller.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, jsonify, request
+from pathlib import Path
+
+import boto3
+from flask import abort, Blueprint, current_app, jsonify, Response, request
 from flask_jwt_extended import jwt_required
 
 from src.main import db
@@ -80,3 +83,96 @@ def delete_user(user, user_id):
     db.session.commit()
 
     return jsonify("The following user was deleted from the database.", user_schema.dump(user))
+
+
+@users.route("/<int:user_id>/image", methods=["POST"])
+@jwt_required
+@verify_user
+def profile_image_create(user, user_id):
+    """
+    Uploads an image to S3 bucket and adds the filename to the profile image column for the user
+
+    Parameters:
+    user: User
+        The user object for the user trying to make the request
+    user_id: integer
+        The user id number for the user to update with a new image
+
+    Returns:
+    Tuple containing message of the request outcome and the response status code
+    """
+
+    if "image" not in request.files:
+        return abort(400, description="No image provided.")
+
+    image = request.files["image"]
+
+    if Path(image.filename).suffix not in (".png", ".jpeg", ".jpg", ".gif"):
+        return abort(400, description="Invalid file type.")
+
+    filename = f"{user.id}.png"
+    bucket = boto3.resource("s3").Bucket(current_app.config["AWS_S3_BUCKET"])
+    key = f"user_profile_images/{filename}"
+    bucket.upload_fileobj(image, key)
+
+    user.profile_image = filename
+    db.session.commit()
+
+    return (f"Profile image set for user: {user.username}", 201)
+
+
+@users.route("/<int:user_id>/image", methods=["GET"])
+def profile_image_show(user, user_id):
+    """
+    Retrieves the profile image for the user id provided
+
+    Parameters:
+    user: User
+        The user object for the user trying to make the request
+    user_id: integer
+        The user id number that we are retrieving the profile image for
+
+    Returns:
+    Response object containing the image and specifies the mimetype
+    """
+
+    if not user.profile_image:
+        return abort(404, description="This user has no profile image.")
+
+    bucket = boto3.resource("s3").Bucket(current_app.config["AWS_S3_BUCKET"])
+    filename = user.profile_image
+    file_obj = bucket.Object(f"user_profile_images/{filename}").get()
+
+    return Response(file_obj["Body"].read(), mimetype="image/*",
+                    headers={"Content-Disposition": "attachment;filename=image"})
+
+
+@users.route("/<int:user_id>/image", methods=["DELETE"])
+@jwt_required
+@verify_user
+def profile_image_delete(user, user_id):
+    """
+    Deletes the profile image from the S3 bucket and the users table data
+    for the user id provided
+
+    Parameters:
+    user: User
+        The user object for the user trying to make the request
+    user_id: integer
+        The user id number that we are deleting the profile image for
+
+    Returns:
+    String containing the request outcome
+    """
+
+    if not user.profile_image:
+        return abort(404, description="This user has no profile image.")
+
+    bucket = boto3.resource("s3").Bucket(current_app.config["AWS_S3_BUCKET"])
+    filename = user.profile_image
+    bucket.Object(f"user_profile_images/{filename}").delete()
+
+    user.profile_image = None
+    db.session.commit()
+
+    return jsonify(f"Successfully removed the profile image for {user.username}.")
